a4c6e936d78d973197a0f7564c9333615fa590a9
Isaac <isaac.bordas@gmail.com>
2018-02-13 22:59:47 +0100

    Added three operators

--- src/MyApp/Bundle/AppBundle/Controller/CalculatorController.php
+++ src/MyApp/Bundle/AppBundle/Controller/CalculatorController.php
@@ -3,7 +3,9 @@
 namespace MyApp\Bundle\AppBundle\Controller;
 
 use MyApp\Component\Calculator\Calculator;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 
 class CalculatorController
 {
@@ -12,6 +14,22 @@ class CalculatorController
     {
         $calculator = new Calculator();
         return new Response((int)$calculator->add($param1, (int)$param2));
+    }
+
+    /**
+     * @Route("/subtract/{param1}/{param2}/", name="subtract", requirements={"param1": "\d+", "param2": "\d+"})
+     */
+    public function subtractAction($param1, $param2)
+    {
+        $calculator = new Calculator();
+        return new Response((int)$calculator->subtract($param1, (int)$param2));
+    }
+
+    public function multiplyAction($param1, Request $request)
+    {
+        $param2 = $request->query->get('param2');
+        $calculator = new Calculator();
+        return new Response((int)$calculator->multiply($param1, (int)$param2));
     }
 
 }
--- src/MyApp/Component/Calculator/Calculator.php
+++ src/MyApp/Component/Calculator/Calculator.php
@@ -10,4 +10,14 @@ class Calculator
         return $v1 + $v2;
     }
 
+    public function subtract(int $v1, int $v2): int
+    {
+        return $v1 - $v2;
+    }
+
+    public function multiply(int $v1, int $v2): int
+    {
+        return $v1 * $v2;
+    }
+
 }
